00202772ed414c55cfd9001c7134e7d532e55f20
Luis-cryp Claros <luis.claros-v@uniminuto.edu.co>
2024-11-02 02:30:57 +0000

    Repaso Semana 13

---
+++ semana11/Semana13/encapsulation_2.py
@@ -0,0 +1,21 @@
+class Person:
+    def __init__(self,name, age):
+        self.__name = name
+        self.__age = age
+    
+    @property 
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
+        self.__age = age
+
+
+person1 = Person("Luisa", 19)
+
+print(person1.age)
+
+person1.age = 17
+
+print(person1.age)
---
+++ semana11/Semana13/heritage_2.py
@@ -0,0 +1,20 @@
+from  abc import ABC
+
+class Person(ABC):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age 
+
+    def __repr__(self):
+        return f"name = {self.name}, age ={self.age}"
+
+
+class Student(Person):
+    def __init__(self, name, age, code):
+        super().__init__(name, age)
+        self.code = code
+
+student1 = Student("Peter", 15,933191)
+print(student1)
+
+
